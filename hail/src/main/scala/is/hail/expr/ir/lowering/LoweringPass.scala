6086f1344f68b8e41e38c7a17b45293fa6bb542b
Chris Vittal <chris@vittal.dev>
2020-12-07 12:50:22 -0500

    DO NOT MERGE: Make optimize always quiet

--- hail/src/main/scala/is/hail/expr/ir/lowering/LoweringPass.scala
+++ hail/src/main/scala/is/hail/expr/ir/lowering/LoweringPass.scala
@@ -25,7 +25,7 @@ case class OptimizePass(_context: String) extends LoweringPass {
   val context = s"optimize: ${_context}"
   val before: IRState = AnyIR
   val after: IRState = AnyIR
-  def transform(ctx: ExecuteContext, ir: BaseIR): BaseIR = Optimize(ir, true, context, ctx)
+  def transform(ctx: ExecuteContext, ir: BaseIR): BaseIR = Optimize(ir, false, context, ctx)
 }
 
 case object LowerMatrixToTablePass extends LoweringPass {
